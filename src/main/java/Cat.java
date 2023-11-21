f06ffc655993d7f70c933512aaf4d07f7070d704
tieschi <tiesch@bk.ru>
2023-11-21 14:28:13 +0300

    Добавил класс Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+
+    String voice;
+
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+}
