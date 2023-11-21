aca5212387996ad76b4388759453dca9a46d3c2c
tieschi <tiesch@bk.ru>
2023-11-21 08:17:38 +0300

    Готовое задание

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat.getVoice());
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1.getVoice());
+
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
+
+
     }
 }
